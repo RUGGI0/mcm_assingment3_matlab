931fc2f1bd84feeb5b9d793b35f405603b42dd20
_6100_ <omegaruggio42@gmail.com>
2025-12-31 16:52:58 +0100

    Refactor inverse kinematics calculations and update tool Jacobian computation

--- src/main.m
+++ src/main.m
@@ -80,12 +80,6 @@ pm.initMotionPlot(t, bTg(1:3,4));
 
 %%%%%%% Kinematic Simulation %%%%%%%
 for i = t
-    % Updating transformation matrices for the new configuration 
-
-    % Get the cartesian error given an input goal frame
-
-    % Update the jacobian matrix of the given model
-
     %% INVERSE KINEMATICS
 
     km = kinematicModel(gm);
@@ -93,21 +87,28 @@ for i = t
 
     x_dot = cc.getCartesianReference(bTg);
 
-    %bJe = km.getJacobianOfJointWrtBase(7);
+    % End-effector Jacobian (base frame), twist order [omega; v]
     bJe = km.J;
 
-    % ---------------------------------
+    % -------------------- PATCHED TOOL JACOBIAN --------------------
+    % End-effector pose
     bTe = gm.getTransformWrtBase(gm.jointNumber);
     bRe = bTe(1:3, 1:3);
-    e_r_tb = bRe' * e_r_te;
 
-    skew_e_r_te = [   0        -e_r_tb(3)  e_r_tb(2);
-                   e_r_tb(3)     0        -e_r_tb(1);
-                  -e_r_tb(2)  e_r_tb(1)     0       ];
-    temp = [eye(3) zeros(3);
-           skew_e_r_te eye(3)];
-    bJt = temp * bJe;
-    % ---------------------------------
+    % EE->Tool offset expressed in base
+    r_bt = bRe * e_r_te;
+
+    % skew(r_bt)
+    S = [   0       -r_bt(3)   r_bt(2);
+          r_bt(3)     0       -r_bt(1);
+         -r_bt(2)   r_bt(1)     0     ];
+
+    % Twist shift: [w_t; v_t] = [I 0; -S I] [w_e; v_e]
+    T = [eye(3) zeros(3);
+         -S     eye(3)];
+
+    bJt = T * bJe;
+    % ---------------------------------------------------------------
 
     % Compute desired joint velocities 
     q_dot = pinv(bJt) * x_dot;
